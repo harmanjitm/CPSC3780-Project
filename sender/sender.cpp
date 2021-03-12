adc3da55843f13fa91e43468bca202fc99c0fe5d
Harmanjit (Harmanjit) Mohaar <mohh3780@53r2d2.cs.uleth.ca>
2021-03-12 16:52:51 -0700

    Sender created to take in command line arguments

--- sender/sender.cpp
+++ sender/sender.cpp
@@ -1,5 +1,21 @@
+using namespace std;
+
 #include <iostream>
 
-int main() {
-  std::cout << "Test Sender\n";
+int main(int argc, char *argv[]) {
+  // Check and see if arguments are appropriate
+  if (argc != 3 && argc != 5) {
+    cout << "Invalid number of arguments.\n";
+  }
+  
+  // Takes in a datafile
+  if (argc == 5) {
+    // Check for file
+  }
+
+  // No datafile - Only Host & Port
+  if (argc == 3) {
+    cout << "Host: " << argv[1] << "\n";
+    cout << "Port: " << argv[2] << "\n";
+  }
 }
